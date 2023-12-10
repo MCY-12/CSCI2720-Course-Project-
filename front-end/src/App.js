9682591a0c46659bca605e0f309507ae0dc155ac
MCY-12 <mcymcy12@gmail.com>
2023-12-10 10:37:54 +0800

    12/10/2023 10:30am
    - Added user profile button
     - To let us list that user's favourites
    
    - Index
     - Added table
     - Added sort for the Number of Events column
     - Finished the functionality of the search filter
      - It now automatically live filters the entries in the table to match what is typed into the search bar
    
    - Location Info
     - Added some things for the structure of what will be in location info
      - Includes description of location, events, title venue etc
    
    Note:
    - Favourites system yet to be coded, need backend team to work on that.
    - The search filter bar works on a "data array" called indexData. Backend team can you link that to your database or something?
    - Can someone make it so that when the window width is under a certain point, the user profile button OffCanvas will take up the entire screen instead of 35%? Thx

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Navbar, Nav, NavDropdown, Offcanvas, Form, InputGroup, FormControl, Button, Container, Stack, Row, Col } from 'react-bootstrap';
+//Import bootstrap classes here
+import { Navbar, Nav, NavDropdown, Table, Dropdown, DropdownButton, Offcanvas, Form, InputGroup, FormControl, Button, Container, Stack, Row, Col } from 'react-bootstrap';
+//Import react stuff here
 import { useState } from 'react';
 
 //import axios from 'axios';
@@ -10,21 +12,57 @@ import EventManagement from './EventManagement';
 import UserManagement from './UserManagement';
 import Login from './login';
 
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-
-
+//Import bootstrap icons here
+import { FilePerson, ForwardFill, FilterCircle, Heart, HeartFill } from 'react-bootstrap-icons';
 
 //https://react-bootstrap.github.io/docs/getting-started/introduction
 //if you need help on how the react bootstrap code is written ^^
 
 function App() {
+  //For the Index offcanvas menu
   const [isOpen, setIsOpen] = useState(false);
-
   const handleClose = () => setIsOpen(false);
   const handleShow = () => setIsOpen(true);
 
+  //For the Profile offcanvas menu
+  const [profileIsOpen, setProfileIsOpen] = useState(false);
+  const handleProfileClose = () => setProfileIsOpen(false);
+  const handleProfileShow = () => setProfileIsOpen(true);
+
+  //For the price filter dropdown
   const [priceFilterTitle, setPriceFilterTitle] = useState("Under");
+
+  //For Index offcanvas table number of events sort
+  const [indexData, setIndexData] = useState([
+    { location: 'Location 1', events: 3 },
+    { location: 'asdfgh', events: 4 },
+    { location: 'testing25 6969', events: 6 },
+    //data here..
+    //backend people link this to your database somehow
+  ]);
+  const [indexNumberOfEventsSortConfig, setIndexNumberOfEventsSortConfig] = useState({ key: 'events', direction: 'ascending' });
+  const [indexSearchTerm, setIndexSearchTerm] = useState(''); 
+
+  const sortedIndexData = [...indexData].sort((a, b) => {
+    if (a[indexNumberOfEventsSortConfig.key] < b[indexNumberOfEventsSortConfig.key]) {
+      return indexNumberOfEventsSortConfig.direction === 'ascending' ? -1 : 1;
+    }
+    if (a[indexNumberOfEventsSortConfig.key] > b[indexNumberOfEventsSortConfig.key]) {
+      return indexNumberOfEventsSortConfig.direction === 'ascending' ? 1 : -1;
+    }
+    return 0;
+  });
+
+  const filteredIndexData = sortedIndexData.filter(item => 
+    item.location.toLowerCase().includes(indexSearchTerm.toLowerCase()));
+
+  const handleIndexNumberOfEventsSort = (key) => {
+    let direction = 'ascending';
+    if (indexNumberOfEventsSortConfig.key === key && indexNumberOfEventsSortConfig.direction === 'ascending') {
+      direction = 'descending';
+    }
+    setIndexNumberOfEventsSortConfig({ key, direction });
+  };  
 
 
   //by 1155174712
@@ -79,11 +117,16 @@ function App() {
     <div className="App">
       <Navbar bg="light" expand="lg">
         <Navbar.Brand className="ms-3" href="#" onClick={handleShow}>☰☰</Navbar.Brand>
-        <Form inline className="me-auto" style={{width: "65%", minWidth: "300px", maxWidth: "600px"}}>
+        <Form inline className="me-auto" style={{width: "52%", minWidth: "250px", maxWidth: "600px"}}>
           <FormControl type="text" placeholder="Search" className="me-sm-2" />
         </Form>
-        <Nav className="ml-auto">
-          <Button className="me-3" variant="outline-success" onClick={handleLogout}>Logout</Button>
+        <Nav className="">
+          <Button className="me-2" variant="outline-danger" onClick={handleLogout}>Logout</Button>
+        </Nav>
+        <Nav className="">
+          <Button className="me-3" variant="outline-dark" onClick={handleProfileShow}>
+            <FilePerson size={24} />
+          </Button>
         </Nav>
       </Navbar>
 
@@ -98,10 +141,41 @@ function App() {
               placeholder="Search"
               className="me-2"
               aria-label="Search"
+              onChange={e => setIndexSearchTerm(e.target.value)}
             />
-            <Button variant="outline-success">Search</Button>
           </Form>
-          Temporary placeholder text. Will have filter/search + table with links in future. Backend people please handle the search code. Can someone help me add in the feature where if the window is sm or smaller then the offcanvas will take up the whole screen instead of 75%? Thanks.
+          Can someone help me add in the feature where if the window is sm or smaller then the offcanvas will take up the whole screen instead of 75%? Thanks.
+          <Table hover striped>
+            <thead>
+              <tr>
+                <th>Location</th>
+                <th>
+                  Number of Events &nbsp;
+                  <FilterCircle size={24} onClick={() => handleIndexNumberOfEventsSort('events')}/></th>
+                <th>Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredIndexData.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.location}</td>
+                  <td>{item.events}</td>
+                  <td><ForwardFill href="#" size={24} /></td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Offcanvas.Body>
+      </Offcanvas>
+
+      <Offcanvas show={profileIsOpen} onHide={handleProfileClose} placement={'end'} style={{width: "35%"}}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Profile</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <h5>Username: (someone make it so that this will show their username please)</h5>
+          <h3>Favourites</h3>
+          <p>List the favourites here</p>
         </Offcanvas.Body>
       </Offcanvas>
 
@@ -136,13 +210,24 @@ function App() {
           </Col>    
       </Container>
       <Container fluid className="mx-3 pb-4 bg-light">
-        <Row>
-          <h3 className="my-2 fw-bold">Location Info:</h3>
-          <p>blah blah blah</p>
+          <h3 className="my-2 pt-2 fw-bold">Location Info: (update this title to whatever the location is called)</h3>
+          <Button className="mx-3" variant="outline-danger" >
+            <Heart size={24} />
+          </Button>
+          <p>Description of location here</p>
           <p>someone set up the google maps api please</p>
-          <p>blah blah blah</p>
-          <p>blah blah blah</p>
-        </Row>
+          <p>Someone help me put the favorite icon in a better position^</p>
+          <Container className="ms-3">
+            <h4 className="mt-4 fw-bold">Events:</h4>
+            <Container className="ms-2">
+              <p>Title: </p>
+              <p>Venue: </p>
+              <p>Date & Time: </p>
+              <p>Description: </p>
+              <p>Presenter: </p>
+              <p>Price: </p>
+            </Container>
+          </Container>
         <Container fluid>
           <Container fluid className="border bg-secondary text-light">
             <Row>
@@ -156,9 +241,6 @@ function App() {
         
         </Container>
       </Container>
-      
-      
-      {isOpen && <Col className="col-4">Menu</Col>}
     </div>
   );
 }
